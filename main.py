85fdd9719e74c30aefe0509e32bae981b0f47c3d
Channo Bebe <channobebe@Channos-MacBook-Air.local>
2025-12-12 17:08:25 -0800

    Fix HTMLResponse import

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-import sqlite3
-from pathlib import Path
-
+from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request
 import sqlite3
 
 def get_db_jobs():
