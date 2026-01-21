80b0fe9c737244f1ca72193f37bd68f8b6118536
Channo Bebe <channobebe@Channos-MacBook-Air.local>
2026-01-21 15:43:22 -0800

    Add home route

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI(title="Win-Win Job")
 
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
 # ----------------------------
 # Static + Templates
 # ----------------------------
